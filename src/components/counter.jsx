5ddbf1299c607afe160c3639e0d0a6e201e19482
robrajcs <robrajcs@gmail.com>
2022-12-26 21:05:07 +0100

    d

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value,
-        tags: []
-    };
-
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 }); 
-    }
-
-    // renderTags() {
-    //     if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-    //     return (         
-    //         <ul>
-    //             { this.state.tags.map(tag => <li key={ tag }>{ tag }</li> ) }
-    //         </ul>)
-    // }
 
     render() { 
         return (
         <div> 
             <span>{this.state.value}</span>
             {/* { this.state.tags.length === 0 && 'Please create a new tag!' } */}
-            <button onClick={this.handleIncrement}>Increment</button>
+            <button onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
             <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
             
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {value} = this.props.counter;
+        return value === 0 ? "Zero" : value;
     }
 
 }
