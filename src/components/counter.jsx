8f5970e8971461d4d8fb67ca30a8e589a22368ea
robrajcs <robrajcs@gmail.com>
2022-12-26 21:38:37 +0100

    20221226 2138

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,7 @@ class Counter extends Component {
     render() { 
         return (
         <div> 
-            <span>{this.state.value}</span>
+            <span>{this.props.counter.value}</span>
             {/* { this.state.tags.length === 0 && 'Please create a new tag!' } */}
             <button onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
             <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
